42c92c4817f6946af7064b530f7a369727cb13b2
Abdul rauf <141711142+Raufjatoi@users.noreply.github.com>
2024-03-06 18:26:32 +0500

    todays c

---
+++ grade.py
@@ -0,0 +1,12 @@
+result = int(input('Whats result: '))
+if 90 >=  result <= 100:
+    print('A')
+# u can use tha or
+elif result >= 80:
+    print('B')
+elif result >= 70:
+    print('C')
+elif result >= 60:
+    print('D')
+else:
+    print('F')
--- hello.py
+++ hello.py
@@ -1,1 +1,2 @@
-# 
+# just wana tell ya bout pow(), strip() , title and cap() diff.
+# now try to rem it lol 
