7571c95f383843379b6e9b142ef3fce2c82306c4
Abdul rauf <141711142+Raufjatoi@users.noreply.github.com>
2024-03-13 13:25:16 +0500

    C

--- emojize.py
+++ emojize.py
@@ -1,15 +1,4 @@
 import emoji
-
-def emojize(p):
-    if '_' in p or ':' in p:
-        return emoji.emojize(p)
-    else:
-        return emoji.emojize(p, use_aliases=True)
-
-def main():
-    emoji_input = input('Input: ')
-    emojized = emojize(emoji_input)
-    print(emojized)
-
-if __name__ == "__main__":
-    main()
+emoji_input = input('Input: ')
+print(f'{emoji.emojize(emoji_input, language="alias")}')
+    
--- figlet.py
+++ figlet.py
@@ -1,14 +1,31 @@
 import sys
+import random
 from pyfiglet import Figlet
 
-figlet = Figlet()
-font_list = getFonts()
-if len(sys.argv) != 3:
-    r = input('Input: ')
-    print(figlet.renderText(r))
-elif sys.argv[1] in ('-f', '--font'):
-    font = sys.argv[2]
-    if sys.argv[2] in list:
-        figlet.setFont(font)
-    r = input('Input: ')
-    print(figlet.renderText(r))
+def print_usage_and_exit():
+    print("Usage: python figlet.py [-f <font>]")
+    sys.exit(1)
+
+def get_random_font():
+    figlet = Figlet()
+    available_fonts = figlet.getFonts()
+    return random.choice(available_fonts)
+
+def main():
+    if len(sys.argv) == 1:
+        font = get_random_font()
+    elif len(sys.argv) == 3 and sys.argv[1] in ('-f', '--font'):
+        font = sys.argv[2]
+        figlet = Figlet(font=font)
+        if font not in figlet.getFonts():
+            print(f"Font '{font}' not found.")
+            sys.exit(1)
+    else:
+        print_usage_and_exit()
+    user_input = input("Enter text: ")
+    figlet = Figlet(font=font)
+    rendered_text = figlet.renderText(user_input)
+    print(rendered_text)
+
+if __name__ == "__main__":
+    main()
